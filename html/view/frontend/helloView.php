875d8268cb9741c2597cbe34ceb377306ce066eb
Corinne54 <viniaker@hotmail.com>
2019-01-28 09:27:05 +0100

    Project initialization MVC Structure

---
+++ html/view/frontend/helloView.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Mon blog</title>
+    <link href="style.css" rel="stylesheet" />
+</head>
+
+<body>
+Hello
+<?=$name; ?>
+</body>
+</html>
